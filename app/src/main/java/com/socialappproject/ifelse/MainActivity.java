bbe82d9f172c76335cb174350049adf9aa6d5455
junseon <junseon@Junseon.local>
2017-10-08 18:33:28 +0900

    login

--- app/src/main/java/com/socialappproject/ifelse/MainActivity.java
+++ app/src/main/java/com/socialappproject/ifelse/MainActivity.java
@@ -1,21 +1,84 @@
 package com.socialappproject.ifelse;
 
 import android.os.SystemClock;
+import android.support.annotation.NonNull;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         endSplash();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
+
+        // show dialog when sign_up button clicked
+        findViewById(R.id.sign_up).setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View v) {
+                        //여기에 이벤트를 적어주세요
+                        showInputID_Dialog();
+                    }
+                }
+        );
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 
     private void endSplash() { // splash 이미지로 앱이 시작하여 2초 유지 후 본래 테마로 복귀
         SystemClock.sleep(2000);
         setTheme(R.style.AppTheme);
+    }
+
+    private void showInputID_Dialog() { // show dialog method
+
+        FragmentManager fm = getSupportFragmentManager();
+        InputID_SignUpFragment inputIDSignUpFragment = InputID_SignUpFragment.newInstance("input_id");
+
+        inputIDSignUpFragment.show(fm, "fragment_input_id");
+
     }
 
 }
